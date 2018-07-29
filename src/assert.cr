288807a2c8ff725714b41b4244212ee321c7cf38
Sam Johnson <sam@durosoft.com>
2018-07-29 17:15:02 -0400

    add to_b method

--- src/assert.cr
+++ src/assert.cr
@@ -1,6 +1,20 @@
-require "./assert/*"
-
-# TODO: Write documentation for `Assert`
 module Assert
-  # TODO: Put your code here
+  def self.to_b(expr)
+    if expr.is_a? Bool
+      return expr
+    elsif expr.nil?
+      return false
+    elsif expr.is_a? Int
+      return expr > 0
+    elsif expr.is_a? Float
+      return expr > 0.0
+    elsif expr.is_a? String
+      return false if expr == ""
+      return Assert.to_b(expr.to_i) if expr.to_i.to_s == expr
+      return true if expr.downcase == "true"
+      return false if expr.downcase == "false"
+      true
+    end
+    false
+  end
 end
--- src/assert/version.cr
+++
@@ -1,3 +0,0 @@
-module Assert
-  VERSION = "0.1.0"
-end
