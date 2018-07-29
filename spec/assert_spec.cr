66e17b00c0c72b5de6d8c4f5162f72d84ea73ef1
Sam Johnson <sam@durosoft.com>
2018-07-29 17:20:04 -0400

    add specs

--- spec/assert_spec.cr
+++ spec/assert_spec.cr
@@ -1,9 +1,37 @@
 require "./spec_helper"
 
-describe Assert do
-  # TODO: Write tests
+describe "#assert!" do
+  it "succeeds when value is true" do
+    (assert! true).should eq true
+  end
 
-  it "works" do
-    false.should eq(true)
+  it "fails when value is false" do
+    expect_raises(Exception) { assert! false }
+  end
+
+  it "works properly with ints" do
+    assert! 1
+    assert! 10
+    expect_raises(Exception) { assert! 0 }
+    expect_raises(Exception) { assert! -1 }
+  end
+
+  it "works properly with floats" do
+    assert! 0.0024
+    assert! 100.0
+    expect_raises(Exception) { assert! 0.0 }
+    expect_raises(Exception) { assert! -0.1 }
+  end
+
+  it "works properly with strings" do
+    assert! "hey"
+    assert! "TrUe"
+    assert! "yay"
+    assert! "1"
+    assert! "1.1"
+    expect_raises(Exception) { assert! "fAlse" }
+    expect_raises(Exception) { assert! "" }
+    expect_raises(Exception) { assert! "0" }
+    expect_raises(Exception) { assert! "0.0" }
   end
 end
